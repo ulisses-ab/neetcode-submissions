5d744bfbdd3d489bf60a2eabec9ca3245bbcc606
ulisses-ab <50384765+ulisses-ab@users.noreply.github.com>
2026-05-25 18:26:22 -0300

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    struct Car {
+        int position;
+        int speed;
+
+        double raw_time(int target) {
+            return double(target - position) / speed;
+        }
+    };
+
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size();
+        vector<Car> cars;
+
+        for (int i = 0; i < n; i++) {
+            cars.push_back(Car{position[i], speed[i]});
+        }
+
+        sort(cars.begin(), cars.end(), [](Car& a, Car& b) { return a.position < b.position; });
+
+        double last = cars.back().raw_time(target);
+
+        int count = 1;
+
+        for (int i = n-2; i >= 0; i--) {
+            double time = cars[i].raw_time(target);
+
+            if (time > last) {
+                last = time;
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
