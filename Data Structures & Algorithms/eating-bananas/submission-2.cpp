31a278a3011edfed170a5aa088263019fe660cff
ulisses-ab <50384765+ulisses-ab@users.noreply.github.com>
2026-05-26 00:08:18 -0300

    Add: eating-bananas - submission-2

---
+++ Data Structures & Algorithms/eating-bananas/submission-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool possible(vector<int>& piles, int h, int k) {
+        int time = 0;
+        
+        for (auto pile : piles) {
+            time += (pile+k-1) / k;
+        }
+
+        if (time > h) return false;
+
+        return true;
+    } 
+
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int lo = 1, hi = 1e9;
+
+        while (lo < hi) {
+            int m = (lo+hi) / 2;
+
+            if (possible(piles, h, m)) {
+                hi = m;
+            }
+            else {
+                lo = m + 1;
+            }
+        }   
+
+        return lo;     
+    }
+};
