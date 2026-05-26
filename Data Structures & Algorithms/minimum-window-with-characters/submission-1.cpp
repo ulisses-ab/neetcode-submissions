bd1c96b0d58b26ef5f80ddb1b7df71b92e29058c
ulisses-ab <50384765+ulisses-ab@users.noreply.github.com>
2026-05-25 21:02:34 -0300

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    string minWindow(string str, string t) {
+        unordered_map<char, int> cnt1, cnt2;
+        int satisfied = 0, goal = 0;
+
+        for (auto c : t) {
+            if (cnt2[c] == 0) goal++;
+            cnt2[c]++;
+        }
+
+        int e = 0;
+        int smallest = 1e9;
+        int best;
+
+        cout << goal << endl;
+
+        for (int s = 0; s < str.size(); s++) {
+            while (satisfied < goal) {
+                if (e == str.size()) break;
+
+                cnt1[str[e]]++;
+                if (cnt1[str[e]] == cnt2[str[e]] && cnt2[str[e]] > 0) satisfied++;
+                e++;
+            }
+
+            if (satisfied < goal && e == str.size()) break;
+
+            int sz = e - s;
+
+            if (sz < smallest) {
+                best = s;
+                smallest = sz;
+            }
+
+            if (cnt1[str[s]] == cnt2[str[s]] && cnt2[str[s]] > 0)  {
+                satisfied--;
+            }
+            cnt1[str[s]]--;
+        }
+
+        return smallest == 1e9 ? "" : str.substr(best, smallest);
+    }
+};
